c9d199f0cad05713799828d342e2a04a363c5db1
Daniil Ten <tendaniil06@gmail.com>
2019-01-19 22:36:45 +0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Тен Даниил on 18/01/2019.
+//  Copyright © 2019 Тен Даниил. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
